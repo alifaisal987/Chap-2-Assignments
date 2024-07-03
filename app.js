e58e05769c729b1144095b7d4727176f0e232b4c
alifaisal987 <alifaisal987@gmail.com>
2024-07-04 00:10:54 +0500

    assignment 2 till q5 donne

---
+++ app.js
@@ -0,0 +1,24 @@
+
+//QUESTION 1
+var userName="alifaisal987";
+
+//QUESTION 2
+var myName ="SYED ALI FAISAL";
+// console.log(myName);
+
+//QUESTION 3
+alert (myName);
+var message="HELLO WORLD";
+alert (message);
+
+//QUESTION 4
+var StudentName="Jhone Doe";
+var StudAge=20;
+
+alert (StudentName);
+alert (StudAge);
+
+// QUESTION 5
+
+var pizza="PIZZA\n PIZZ\n PIZ\n PI\n P";
+alert(pizza);
